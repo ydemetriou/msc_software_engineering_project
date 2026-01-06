08b0aea93a1c716e7a8f6692aed45b400f2993dc
ydemetriou <yiannis.dimitriou@learnworlds.com>
2026-01-06 20:33:49 +0200

    insert categories

--- spring-app/src/main/java/com/cooking-recipe-project/domain/model/Step.java
+++ spring-app/src/main/java/com/cooking-recipe-project/domain/model/Step.java
@@ -2,4 +2,52 @@ package com.cooking.recipe.project.domain.model;
 
 public class Step {
 
+    private Long id;
+
+    private String title;
+
+    private String description;
+
+    private Long duration;
+
+    public Step() {
+    }
+
+    public Step(String title, String description, Long duration) {
+        this.title = title;
+        this.description = description;
+        this.duration = duration;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Long getDuration() {
+        return duration;
+    }
+
+    public void setDuration(Long duration) {
+        this.duration = duration;
+    }
 }
