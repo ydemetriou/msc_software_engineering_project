ab5ae8e7bb03f48b1334a3b4fdfc7cb255fbd412
Alex <std107881@ac.eap.gr>
2026-01-08 14:24:16 +0200

    repositories

--- spring-app/src/main/java/com/cooking-recipe-project/domain/repository/CategoryRepository.java
+++ spring-app/src/main/java/com/cooking-recipe-project/domain/repository/CategoryRepository.java
@@ -1,4 +1,16 @@
 package com.cooking.recipe.project.domain.repository;
 
+import com.cooking.recipe.project.domain.model.Ingredient;
+import com.cooking.recipe.project.domain.model.Category;
+import com.cooking.recipe.project.domain.model.Recipe;
+import com.cooking.recipe.project.domain.model.Photo;
+import com.cooking.recipe.project.domain.model.Step;
+import java.util.List;
+
 public interface CategoryRepository {
+    Category findById(Long id);
+    List<Category> findAll();
+    Category findByName(String name);
+    Category save(Category category);
+    void delete(Long id);
 }
--- spring-app/src/main/java/com/cooking-recipe-project/domain/repository/StepRepository.java
+++ spring-app/src/main/java/com/cooking-recipe-project/domain/repository/StepRepository.java
@@ -1,4 +1,11 @@
 package com.cooking.recipe.project.domain.repository;
 
+import com.cooking.recipe.project.domain.model.Ingredient;
+import com.cooking.recipe.project.domain.model.Category;
+import com.cooking.recipe.project.domain.model.Recipe;
+import com.cooking.recipe.project.domain.model.Photo;
+import com.cooking.recipe.project.domain.model.Step;
+import java.util.List;
+
 public interface StepRepository {
 }
